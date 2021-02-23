ed7fef7f119ad423434eedd7fd47ec738589d9b0
EugeneLiu <liuyichen.destiny@bytedance.com>
2021-02-23 21:19:38 +0800

    add: 增加测试与注释

--- src/main/scala/lexer/Lexer.scala
+++ src/main/scala/lexer/Lexer.scala
@@ -3,6 +3,7 @@ package lexer
 object Lexer {
   // span 使用bool表达式进行切割，遇到第一个不满足的元素就返回，形成两个List，从开头至中断处满足bool表达式的第一个List，以及从不满足bool表达式的第一个元素至之前列表最后一个元素构成的List
   // 在这里取跳过的所有无效字符的新List作为结果返回
+  // 确保从字符串中取字符的时候 其之前的无效字符都已经被滤掉
   def skipWhiteSpace(sourceCharList:List[Char]) :List[Char] = sourceCharList.span(_.isWhitespace)._2
 
   //针对@tailrec注解的使用需要注意
--- src/test/scala/lexer/LexerTest.scala
+++ src/test/scala/lexer/LexerTest.scala
@@ -6,7 +6,7 @@ class LexerTest extends AnyFunSuite {
 
   test("skipWhiteSpace()") {
     //    assert("XXXXXX" == "YYYYYYYYYY")
-    val source = List('X', 'A', '\n', 'S', 'Q')
+    val source = List('\n','X', 'A', '\n', 'S', 'Q')
     println(source.span(_.isWhitespace))
     println(source.span(_.isWhitespace)._1)
     println(source.span(_.isWhitespace)._2)
