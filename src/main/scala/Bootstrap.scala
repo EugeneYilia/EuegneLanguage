91a5e8e5badb6bc4904df041371145beb3dd4ef3
EugeneLiu <liuyichen.destiny@bytedance.com>
2021-02-24 21:09:30 +0800

    add: 文件读取 部分词法分析

--- src/main/scala/Bootstrap.scala
+++ src/main/scala/Bootstrap.scala
@@ -1,3 +1,9 @@
+import scala.io.Source
+
 object Bootstrap extends App{
-  println("Compiler Start")
+  println(args(0))
+  val file = Source.fromFile(args(0))
+  val fileContent = file.mkString
+  println(fileContent)
+
 }
--- src/main/scala/common/package.scala
+++ src/main/scala/common/package.scala
@@ -1,3 +1,5 @@
+import common.SyntacticSymbol.SyntacticSymbol
+
 package object common {
   object SyntacticSymbol extends Enumeration {
     type SyntacticSymbol = Value
@@ -8,9 +10,17 @@ package object common {
     val PLUS,ASSIGN,MINUS,MULTI,LE,GE,LT,GT,EQ = Value
     //      ;       ,
     val SEMICOLON,COMMA = Value
-    val ID = Value
+    val ID : SyntacticSymbol = Value
     //      （          ）               [                  ]               {            }
     val LEFT_PAREN,RIGHT_PAREN,LEFT_SQUARE_BRACKET,RIGHT_SQUARE_BRACKET,LEFT_BRACE,RIGHT_BRACE = Value
 
+    val terminalSymbolSet = Set($,INT_KEYWORD,FUNCTION_KEYWORD,PLUS,ASSIGN,MINUS,MULTI,LE,GE,LT,GT,EQ,SEMICOLON,
+      COMMA,ID,LEFT_PAREN,RIGHT_PAREN,LEFT_SQUARE_BRACKET,RIGHT_SQUARE_BRACKET,LEFT_BRACE,RIGHT_BRACE)
+    val nonTerminalSymbolSet = Set(STARTER,FUNCTIONS,FUNCTION,STATEMENTS,STATEMENT,EXPRESSION,BLOCK)
+
+    def isTerminalSymbol(syntacticSymbol: SyntacticSymbol) : Boolean = terminalSymbolSet.contains(syntacticSymbol)
+    def isNonTerminalSymbol(syntacticSymbol: SyntacticSymbol) : Boolean = nonTerminalSymbolSet.contains(syntacticSymbol)
   }
+
+  type Token = (SyntacticSymbol,String)
 }
