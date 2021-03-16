3cf3758af29a9ad1645f3852e7ef418826f9b5b9
EugeneLiu <liuyichen.destiny@bytedance.com>
2021-03-16 11:46:48 +0800

    feat: 添加方法注释

--- src/main/scala/utils/ClosureUtil.scala
+++ src/main/scala/utils/ClosureUtil.scala
@@ -9,6 +9,7 @@ object ClosureUtil {
   //                       STARTER        IntermediateVec       DerivationFirst       DerivationLeft       TerminalSymbol
   type MatchResult = (SyntacticSymbol, Vector[SyntacticSymbol], SyntacticSymbol, Vector[SyntacticSymbol], SyntacticSymbol)
 
+  // 该方法与IntermediateVec集合无关
   def resolve(item: Item): Option[MatchResult] = item match {
     // 当Derivation不止有一个元素的时候会进入到第一个Case
     case (starter, intermediate, derivationFirst +: derivationLeft, terminalSymbol) if (isNonTerminalSymbol(derivationFirst) && isTerminalSymbol(terminalSymbol)) =>
@@ -17,6 +18,7 @@ object ClosureUtil {
   }
 
   // 计算由MatchResult中的derivationFirst非终结符所能产生的所有项目集
+  // 该方法与IntermediateVec集合无关
   def singleStep(item: Item): Closure = {
     resolve(item) match {
       case Some(matchResult: MatchResult) =>
