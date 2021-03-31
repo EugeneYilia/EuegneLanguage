eea10efc1ad4e30ce0606cba830b0869be355a50
EugeneLiu <liuyichen.destiny@bytedance.com>
2021-03-31 11:59:39 +0800

    feat: 增加更多的程序

--- src/main/scala/common/ASTNode/nonTerminalNode/StatementsNode.scala
+++ src/main/scala/common/ASTNode/nonTerminalNode/StatementsNode.scala
@@ -2,10 +2,10 @@ package common.ASTNode.nonTerminalNode
 
 import common.ASTNode.{Env, ExecResult, Node}
 
-case class StatementsNode(statementList:List[StatementNode]) extends Node {
+case class StatementsNode(statementList: List[StatementNode]) extends Node {
   override def exec(env: Env): Option[ExecResult] = {
-    val lastIndex = statementList.size -1
-    for(index <- 0 until lastIndex){
+    val lastIndex = statementList.size - 1
+    for (index <- 0 until lastIndex) {
       statementList(index).exec(env)
     }
     statementList(lastIndex).exec(env)
